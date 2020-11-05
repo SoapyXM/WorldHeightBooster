eadcd52cd9317062b6eca87d7d42dcb75f4e6c50
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2020-11-05 13:51:27 -0800

    Config thingsss

--- src/main/java/dev/soapy/worldheightbooster/WorldHeightBooster.java
+++ src/main/java/dev/soapy/worldheightbooster/WorldHeightBooster.java
@@ -1,18 +1,26 @@
 package dev.soapy.worldheightbooster;
 
+import dev.soapy.worldheightbooster.config.WorldHeightConfig;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 
+import java.nio.file.Path;
 import java.util.logging.Logger;
 
 public class WorldHeightBooster implements ModInitializer {
     // replace this with config when config mods exist again
-    public static final int WORLD_HEIGHT = 768;
+    public static int WORLD_HEIGHT = 768;
     public static final int MIN_Y = 0;
+    public static final String MOD_ID = "worldheightbooster";
 
     public static final Logger LOGGER = Logger.getLogger("worldheightbooster");
+
+    public static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDirectory().toPath();
+
 
     @Override
     public void onInitialize() {
         LOGGER.warning("This mod is experimental. Use it at your own risk.");
+        WorldHeightConfig.handleConfig(CONFIG_PATH.resolve(MOD_ID + ".json"));
     }
 }
---
+++ src/main/java/dev/soapy/worldheightbooster/config/ConfigHolder.java
@@ -0,0 +1,16 @@
+package dev.soapy.worldheightbooster.config;
+
+public class ConfigHolder {
+
+
+    private final int worldHeight;
+
+    public ConfigHolder(int worldHeight) {
+        this.worldHeight = worldHeight;
+    }
+
+    public int getWorldHeight() {
+        return worldHeight;
+    }
+
+}
