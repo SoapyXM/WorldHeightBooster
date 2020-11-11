f91c4818e5f599ecae907d16831e4e0d5a86e77d
SoapyXM <44278296+SoapyXM@users.noreply.github.com>
2020-11-11 18:11:03 -0500

    Removed a hacky workaround in favor of more vanilla-like behavior.

--- src/main/java/dev/soapy/worldheightbooster/mixin/worldgen/sealevel/MixinBadlandsSurfaceBuilder.java
+++ src/main/java/dev/soapy/worldheightbooster/mixin/worldgen/sealevel/MixinBadlandsSurfaceBuilder.java
@@ -1,16 +1,22 @@
 package dev.soapy.worldheightbooster.mixin.worldgen.sealevel;
 
 import dev.soapy.worldheightbooster.WorldHeightBooster;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.levelgen.surfacebuilders.BadlandsSurfaceBuilder;
+import net.minecraft.world.level.levelgen.surfacebuilders.SurfaceBuilderBaseConfiguration;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+import java.util.Random;
 
 @Mixin(BadlandsSurfaceBuilder.class)
 public class MixinBadlandsSurfaceBuilder {
     //TODO: find a way to do this more elegantly
     @ModifyConstant(method = "apply(Ljava/util/Random;Lnet/minecraft/world/level/chunk/ChunkAccess;Lnet/minecraft/world/level/biome/Biome;IIIDLnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/block/state/BlockState;IJLnet/minecraft/world/level/levelgen/surfacebuilders/SurfaceBuilderBaseConfiguration;)V", constant = @Constant(intValue = 64))
-    public int properSeaLevel(int oldValue) {
-        return (int) (WorldHeightBooster.WORLD_HEIGHT * 0.63);
+    public int properSeaLevel(int oldValue, Random random, ChunkAccess chunkAccess, Biome biome, int i, int j, int k, double d, BlockState blockState, BlockState blockState2, int l, long m, SurfaceBuilderBaseConfiguration surfaceBuilderBaseConfiguration) {
+        return l;
     }
 }
